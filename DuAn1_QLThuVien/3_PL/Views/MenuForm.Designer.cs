4f1e95b0a9c147eaa3c1c5406d1878f3220b2525
Minh Nguyen <minhnguyen16082001@gmail.com>
2022-11-15 23:25:32 +0700

    s

--- DuAn1_QLThuVien/3_PL/Views/MenuForm.Designer.cs
+++ DuAn1_QLThuVien/3_PL/Views/MenuForm.Designer.cs
@@ -31,16 +31,12 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pn_body = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.lb_thoigian = new System.Windows.Forms.Label();
             this.button7 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_muontra = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2.SuspendLayout();
@@ -50,28 +46,35 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // panel1
+            // pn_body
             // 
-            this.panel1.Location = new System.Drawing.Point(219, 100);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(849, 449);
-            this.panel1.TabIndex = 1;
+            this.pn_body.Location = new System.Drawing.Point(219, 100);
+            this.pn_body.Name = "pn_body";
+            this.pn_body.Size = new System.Drawing.Size(849, 449);
+            this.pn_body.TabIndex = 1;
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.lb_thoigian);
             this.panel2.Controls.Add(this.button7);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.button6);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btn_muontra);
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Location = new System.Drawing.Point(12, 12);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(201, 537);
             this.panel2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(0, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 46);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Mượn Sách - Trả Sách";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // lb_thoigian
             // 
@@ -92,59 +95,16 @@
             this.button7.Text = "ĐĂNG XUẤT";
             this.button7.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btn_muontra
             // 
-            this.button4.Location = new System.Drawing.Point(0, 356);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(201, 46);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(0, 316);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(201, 46);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(0, 276);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(201, 46);
-            this.button6.TabIndex = 4;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(0, 233);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(201, 46);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 193);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(201, 46);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 153);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(201, 46);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_muontra.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btn_muontra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_muontra.Location = new System.Drawing.Point(0, 153);
+            this.btn_muontra.Name = "btn_muontra";
+            this.btn_muontra.Size = new System.Drawing.Size(201, 46);
+            this.btn_muontra.TabIndex = 1;
+            this.btn_muontra.Text = "Mượn Sách - Trả Sách";
+            this.btn_muontra.UseVisualStyleBackColor = false;
             // 
             // panel4
             // 
@@ -172,7 +132,7 @@
             this.ClientSize = new System.Drawing.Size(1080, 561);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pn_body);
             this.Name = "MenuForm";
             this.Text = "Menu";
             this.panel2.ResumeLayout(false);
@@ -183,17 +143,13 @@
 
         #endregion
         private System.Windows.Forms.Timer timer1;
-        private Panel panel1;
+        private Panel pn_body;
         private Panel panel2;
         private Panel panel3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button3;
-        private Button button2;
-        private Button button1;
+        private Button btn_muontra;
         private Panel panel4;
         private Label lb_thoigian;
         private Button button7;
+        private Button button2;
     }
 }
