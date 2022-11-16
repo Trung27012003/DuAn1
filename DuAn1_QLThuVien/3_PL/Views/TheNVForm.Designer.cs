256d1ab0f647e4fd35d1c2fe8b6e53e7295d239b
Minh Nguyen <minhnguyen16082001@gmail.com>
2022-11-16 23:29:39 +0700

    Sua 16/11 lan 4

--- DuAn1_QLThuVien/3_PL/Views/TheNVForm.Designer.cs
+++ DuAn1_QLThuVien/3_PL/Views/TheNVForm.Designer.cs
@@ -70,7 +70,7 @@
             this.tabControl1.Location = new System.Drawing.Point(1, -1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(922, 513);
+            this.tabControl1.Size = new System.Drawing.Size(922, 546);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -95,7 +95,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(914, 480);
+            this.tabPage1.Size = new System.Drawing.Size(914, 513);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Nhân viên";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -133,7 +133,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(901, 203);
+            this.dataGridView1.Size = new System.Drawing.Size(901, 238);
             this.dataGridView1.TabIndex = 13;
             // 
             // button3
@@ -254,7 +254,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(825, 457);
+            this.tabPage2.Size = new System.Drawing.Size(914, 480);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Chức vụ";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -316,7 +316,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 515);
+            this.ClientSize = new System.Drawing.Size(925, 545);
             this.Controls.Add(this.tabControl1);
             this.Name = "TheNVForm";
             this.Text = "TheTVForm";
