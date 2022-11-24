2be4bb102bd458ce43420bc43bb1b1af1329f683
Trung27012003 <trungtvph28671@fpt.edu.vn>
2022-11-24 11:32:14 +0700

    update

--- DuAn1_QLThuVien/1_DAL/Models/PhieuMuonChiTiet.cs
+++ DuAn1_QLThuVien/1_DAL/Models/PhieuMuonChiTiet.cs
@@ -14,6 +14,7 @@ namespace _1_DAL.Models
         public int? SoLuong { get; set; }
         public string? DieuKien { get; set; }
         public string? GhiChu { get; set; }
+        public int? TienTheChan { get; set; }
         public virtual Sach Sach { get; set; }
         public virtual PhieuMuon PhieuMuon { get; set; }
 
