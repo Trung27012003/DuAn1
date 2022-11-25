7dc1e3db650d0cc9ff262ff3430028007aeaab2a
Minh Nguyen <minhnguyen16082001@gmail.com>
2022-11-25 07:36:18 +0700

    Rep Update lan 1

--- DuAn1_QLThuVien/1_DAL/IRespositories/ITheNgayRep.cs
+++ DuAn1_QLThuVien/1_DAL/IRespositories/ITheNgayRep.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using _1_DAL.Models;
 namespace _1_DAL.IRespositories
 {
-    internal interface TheThanhVien
+    public interface ITheNgayRep
     {
+        bool AddTN(TheNgay x);
+
+        bool RemoveTN(TheNgay x);
+
+        bool UpdateTN(TheNgay x);
+
+        List<TheNgay> GetAllTNs();
     }
 }
