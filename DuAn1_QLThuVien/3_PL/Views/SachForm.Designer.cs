3b5377104fed71703ca8fda1b6d37aeaff9c8b5d
NXHoangVu <vunxhph19692@fpt.edu.vn>
2022-12-02 00:13:49 +0700

    Sách

--- DuAn1_QLThuVien/3_PL/Views/SachForm.Designer.cs
+++ DuAn1_QLThuVien/3_PL/Views/SachForm.Designer.cs
@@ -32,20 +32,20 @@
             this.tbt_giatien = new System.Windows.Forms.TextBox();
             this.tbt_tensach = new System.Windows.Forms.TextBox();
             this.tbt_soluong = new System.Windows.Forms.TextBox();
-            this.cbb_tl = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.ccb_nxb = new System.Windows.Forms.ComboBox();
             this.btn_them = new System.Windows.Forms.Button();
             this.btn_sua = new System.Windows.Forms.Button();
             this.btn_xoa = new System.Windows.Forms.Button();
             this.tbt_timkiem = new System.Windows.Forms.TextBox();
             this.dtg_showtl = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbt_NXB = new System.Windows.Forms.TextBox();
+            this.tbt_TL = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.dtg_showsach = new System.Windows.Forms.DataGridView();
@@ -60,120 +60,91 @@
             // 
             // tbt_tg
             // 
-            this.tbt_tg.Location = new System.Drawing.Point(138, 95);
-            this.tbt_tg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbt_tg.Location = new System.Drawing.Point(110, 76);
             this.tbt_tg.Name = "tbt_tg";
-            this.tbt_tg.Size = new System.Drawing.Size(272, 31);
+            this.tbt_tg.Size = new System.Drawing.Size(218, 27);
             this.tbt_tg.TabIndex = 1;
             // 
             // tbt_giatien
             // 
-            this.tbt_giatien.Location = new System.Drawing.Point(138, 321);
-            this.tbt_giatien.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbt_giatien.Location = new System.Drawing.Point(110, 257);
             this.tbt_giatien.Name = "tbt_giatien";
-            this.tbt_giatien.Size = new System.Drawing.Size(272, 31);
+            this.tbt_giatien.Size = new System.Drawing.Size(218, 27);
             this.tbt_giatien.TabIndex = 4;
             // 
             // tbt_tensach
             // 
-            this.tbt_tensach.Location = new System.Drawing.Point(138, 211);
-            this.tbt_tensach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbt_tensach.Location = new System.Drawing.Point(110, 169);
             this.tbt_tensach.Name = "tbt_tensach";
-            this.tbt_tensach.Size = new System.Drawing.Size(272, 31);
+            this.tbt_tensach.Size = new System.Drawing.Size(218, 27);
             this.tbt_tensach.TabIndex = 5;
             // 
             // tbt_soluong
             // 
-            this.tbt_soluong.Location = new System.Drawing.Point(138, 270);
-            this.tbt_soluong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbt_soluong.Location = new System.Drawing.Point(110, 216);
             this.tbt_soluong.Name = "tbt_soluong";
-            this.tbt_soluong.Size = new System.Drawing.Size(272, 31);
+            this.tbt_soluong.Size = new System.Drawing.Size(218, 27);
             this.tbt_soluong.TabIndex = 6;
-            // 
-            // cbb_tl
-            // 
-            this.cbb_tl.FormattingEnabled = true;
-            this.cbb_tl.Location = new System.Drawing.Point(138, 35);
-            this.cbb_tl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.cbb_tl.Name = "cbb_tl";
-            this.cbb_tl.Size = new System.Drawing.Size(272, 33);
-            this.cbb_tl.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 45);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(23, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.Size = new System.Drawing.Size(62, 20);
             this.label1.TabIndex = 8;
             this.label1.Text = "Thể loại";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 104);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(23, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.Size = new System.Drawing.Size(55, 20);
             this.label2.TabIndex = 9;
             this.label2.Text = "Tác giả";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 160);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(23, 128);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 25);
+            this.label3.Size = new System.Drawing.Size(38, 20);
             this.label3.TabIndex = 10;
             this.label3.Text = "NXB";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(25, 215);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(20, 172);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 25);
+            this.label4.Size = new System.Drawing.Size(65, 20);
             this.label4.TabIndex = 11;
             this.label4.Text = "Tên sách";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(25, 274);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(20, 219);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 25);
+            this.label5.Size = new System.Drawing.Size(69, 20);
             this.label5.TabIndex = 12;
             this.label5.Text = "Số lượng";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(29, 321);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(23, 257);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(71, 25);
+            this.label6.Size = new System.Drawing.Size(60, 20);
             this.label6.TabIndex = 13;
             this.label6.Text = "Giá tiền";
             // 
-            // ccb_nxb
-            // 
-            this.ccb_nxb.FormattingEnabled = true;
-            this.ccb_nxb.Location = new System.Drawing.Point(138, 150);
-            this.ccb_nxb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.ccb_nxb.Name = "ccb_nxb";
-            this.ccb_nxb.Size = new System.Drawing.Size(272, 33);
-            this.ccb_nxb.TabIndex = 16;
-            // 
             // btn_them
             // 
-            this.btn_them.Location = new System.Drawing.Point(894, 39);
-            this.btn_them.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_them.Location = new System.Drawing.Point(715, 31);
             this.btn_them.Name = "btn_them";
-            this.btn_them.Size = new System.Drawing.Size(181, 66);
+            this.btn_them.Size = new System.Drawing.Size(145, 53);
             this.btn_them.TabIndex = 17;
             this.btn_them.Text = "THÊM";
             this.btn_them.UseVisualStyleBackColor = true;
@@ -181,74 +152,83 @@
             // 
             // btn_sua
             // 
-            this.btn_sua.Location = new System.Drawing.Point(894, 130);
-            this.btn_sua.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_sua.Location = new System.Drawing.Point(715, 104);
             this.btn_sua.Name = "btn_sua";
-            this.btn_sua.Size = new System.Drawing.Size(181, 66);
+            this.btn_sua.Size = new System.Drawing.Size(145, 53);
             this.btn_sua.TabIndex = 18;
             this.btn_sua.Text = "SỬA";
             this.btn_sua.UseVisualStyleBackColor = true;
+            this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
             // 
             // btn_xoa
             // 
-            this.btn_xoa.Location = new System.Drawing.Point(894, 230);
-            this.btn_xoa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_xoa.Location = new System.Drawing.Point(715, 184);
             this.btn_xoa.Name = "btn_xoa";
-            this.btn_xoa.Size = new System.Drawing.Size(181, 66);
+            this.btn_xoa.Size = new System.Drawing.Size(145, 53);
             this.btn_xoa.TabIndex = 19;
             this.btn_xoa.Text = "XÓA";
             this.btn_xoa.UseVisualStyleBackColor = true;
+            this.btn_xoa.Click += new System.EventHandler(this.btn_xoa_Click);
             // 
             // tbt_timkiem
             // 
-            this.tbt_timkiem.Location = new System.Drawing.Point(8, 32);
-            this.tbt_timkiem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbt_timkiem.Location = new System.Drawing.Point(6, 26);
             this.tbt_timkiem.Name = "tbt_timkiem";
-            this.tbt_timkiem.Size = new System.Drawing.Size(334, 31);
+            this.tbt_timkiem.Size = new System.Drawing.Size(268, 27);
             this.tbt_timkiem.TabIndex = 20;
             // 
             // dtg_showtl
             // 
             this.dtg_showtl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtg_showtl.Location = new System.Drawing.Point(8, 34);
-            this.dtg_showtl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtg_showtl.Location = new System.Drawing.Point(6, 27);
             this.dtg_showtl.Name = "dtg_showtl";
             this.dtg_showtl.RowHeadersWidth = 51;
             this.dtg_showtl.RowTemplate.Height = 29;
-            this.dtg_showtl.Size = new System.Drawing.Size(259, 339);
+            this.dtg_showtl.Size = new System.Drawing.Size(207, 271);
             this.dtg_showtl.TabIndex = 22;
+            this.dtg_showtl.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg_showtl_CellClick);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.tbt_NXB);
+            this.groupBox1.Controls.Add(this.tbt_TL);
             this.groupBox1.Controls.Add(this.tbt_tg);
             this.groupBox1.Controls.Add(this.tbt_giatien);
             this.groupBox1.Controls.Add(this.tbt_tensach);
             this.groupBox1.Controls.Add(this.tbt_soluong);
-            this.groupBox1.Controls.Add(this.cbb_tl);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.ccb_nxb);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Location = new System.Drawing.Point(306, 14);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(245, 11);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(481, 381);
+            this.groupBox1.Size = new System.Drawing.Size(385, 305);
             this.groupBox1.TabIndex = 23;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chi tiết";
             // 
+            // tbt_NXB
+            // 
+            this.tbt_NXB.Location = new System.Drawing.Point(110, 125);
+            this.tbt_NXB.Name = "tbt_NXB";
+            this.tbt_NXB.Size = new System.Drawing.Size(218, 27);
+            this.tbt_NXB.TabIndex = 18;
+            // 
+            // tbt_TL
+            // 
+            this.tbt_TL.Location = new System.Drawing.Point(110, 33);
+            this.tbt_TL.Name = "tbt_TL";
+            this.tbt_TL.Size = new System.Drawing.Size(218, 27);
+            this.tbt_TL.TabIndex = 17;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.dtg_showtl);
-            this.groupBox2.Location = new System.Drawing.Point(15, 15);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(284, 380);
+            this.groupBox2.Size = new System.Drawing.Size(227, 304);
             this.groupBox2.TabIndex = 24;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Thể Loại";
@@ -256,11 +236,9 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.dtg_showsach);
-            this.groupBox3.Location = new System.Drawing.Point(15, 402);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Location = new System.Drawing.Point(12, 322);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox3.Size = new System.Drawing.Size(1130, 260);
+            this.groupBox3.Size = new System.Drawing.Size(904, 208);
             this.groupBox3.TabIndex = 25;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Danh sách";
@@ -268,32 +246,29 @@
             // dtg_showsach
             // 
             this.dtg_showsach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtg_showsach.Location = new System.Drawing.Point(8, 32);
-            this.dtg_showsach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtg_showsach.Location = new System.Drawing.Point(6, 26);
             this.dtg_showsach.Name = "dtg_showsach";
             this.dtg_showsach.RowHeadersWidth = 51;
             this.dtg_showsach.RowTemplate.Height = 29;
-            this.dtg_showsach.Size = new System.Drawing.Size(1115, 220);
+            this.dtg_showsach.Size = new System.Drawing.Size(892, 176);
             this.dtg_showsach.TabIndex = 0;
             // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.tbt_timkiem);
-            this.groupBox4.Location = new System.Drawing.Point(795, 315);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Location = new System.Drawing.Point(636, 252);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox4.Size = new System.Drawing.Size(350, 80);
+            this.groupBox4.Size = new System.Drawing.Size(280, 64);
             this.groupBox4.TabIndex = 26;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Tìm kiếm";
             // 
             // SachForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1156, 678);
+            this.ClientSize = new System.Drawing.Size(925, 542);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -301,9 +276,9 @@
             this.Controls.Add(this.btn_xoa);
             this.Controls.Add(this.btn_sua);
             this.Controls.Add(this.btn_them);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SachForm";
             this.Text = "SachForm";
+            this.Load += new System.EventHandler(this.SachForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtg_showtl)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -321,14 +296,12 @@
         private TextBox tbt_giatien;
         private TextBox tbt_tensach;
         private TextBox tbt_soluong;
-        private ComboBox cbb_tl;
         private Label label1;
         private Label label2;
         private Label label3;
         private Label label4;
         private Label label5;
         private Label label6;
-        private ComboBox ccb_nxb;
         private Button btn_them;
         private Button btn_sua;
         private Button btn_xoa;
@@ -339,5 +312,7 @@
         private GroupBox groupBox3;
         private DataGridView dtg_showsach;
         private GroupBox groupBox4;
+        private TextBox tbt_NXB;
+        private TextBox tbt_TL;
     }
 }
--- DuAn1_QLThuVien/3_PL/Views/SachForm.cs
+++ DuAn1_QLThuVien/3_PL/Views/SachForm.cs
@@ -1,5 +1,6 @@
 ﻿using _2_BUS.IServices;
 using _2_BUS.Services;
+using _2_BUS.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,13 +16,93 @@ namespace _3_PL.Views
     public partial class SachForm : Form
     {
         public ISachServices _IsachServices;
+        private int stt;
+        private Guid _id;
         public SachForm()
         {
             InitializeComponent();
             _IsachServices = new SachServices();
         }
-
+        private void LoadDS()
+        {
+            stt = 1;
+            dtg_showsach.Rows.Clear();
+            dtg_showsach.ColumnCount = 8;
+            dtg_showsach.Columns[0].Name = "STT";
+            dtg_showsach.Columns[1].Name = "Id";
+            dtg_showsach.Columns[2].Name = "Thể loại";
+            dtg_showsach.Columns[3].Name = "Tác giả";
+            dtg_showsach.Columns[4].Name = "NXB";
+            dtg_showsach.Columns[5].Name = "Tên sách";
+            dtg_showsach.Columns[6].Name = "Số lượng";
+            dtg_showsach.Columns[7].Name = "Giá tiền";
+            dtg_showsach.Columns[1].Visible = false;
+            foreach (var x in _IsachServices.GetSach())
+            {
+                dtg_showsach.Rows.Add(
+                    stt++,x.Id,x.TL,x.TG,x.NXB,x.Name,x.SoLuong,x.GiaTien);
+            }
+        }
+        private void LoadTL()
+        {
+            dtg_showtl.Rows.Clear();
+            dtg_showtl.ColumnCount = 1;
+            dtg_showtl.Columns[1].Name = "Thể loại";
+            foreach(var x in _IsachServices.GetSach())
+            {
+                dtg_showtl.Rows.Add(x.TL);
+            }
+        }
+        private SachView GetData()
+        {
+            SachView sachView = new SachView();
+            {
+                sachView.Id = _id;
+                sachView.TL = tbt_TL.Text;
+                sachView.TG = tbt_tg.Text;
+                sachView.NXB = tbt_NXB.Text;
+                sachView.Name = tbt_tensach.Text;
+                sachView.SoLuong =Convert.ToInt32(tbt_soluong.Text);
+                sachView.GiaTien = Convert.ToInt32(tbt_giatien.Text);
+            }return sachView;
+            
+        }
+        private void CheckNhap()
+        {
+            
+        }
         private void btn_them_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Bạn có muốn thêm sách mới không?", "Xác nhận", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                MessageBox.Show(_IsachServices.AddTN(GetData()));
+                LoadDS();
+            }
+            else
+            {
+                MessageBox.Show("Đã hủy");
+            }
+        }
+
+        private void dtg_showtl_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbt_TL.Text = dtg_showtl.CurrentCell.Value.ToString();
+        }
+
+        private void SachForm_Load(object sender, EventArgs e)
+        {
+            LoadTL();
+            LoadDS();
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Bạn có muốn thêm sách mới không?", "Xác nhận", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes) { }
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
         {
 
         }
