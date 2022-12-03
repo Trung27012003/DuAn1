64482faaa503de713c6464865f993e268ea70fd7
Trung27012003 <git@github.com:Trung27012003/DuAn1.git>
2022-12-03 21:38:23 +0700

    add check validate sdt

--- DuAn1_QLThuVien/2_BUS/Utilities/Validates.cs
+++ DuAn1_QLThuVien/2_BUS/Utilities/Validates.cs
@@ -10,11 +10,30 @@ namespace _2_BUS.Utilities
     public class Validates
     {
 
-        public bool checkSo(string text)
+        public string checkSDT(string s)
         {
-            // return Regex.IsMatch(text, "^[0-9]*$");
-            return Regex.IsMatch(text, "\\d+");
+            bool startWith0 = s.StartsWith("0");
+            bool isNumber = Regex.IsMatch(s, @"^\d+$");
+            bool isLength = s.Length == 10;
+            bool isLetter = Regex.IsMatch(s, @"^[a-zA-Z]+$");
+            bool isEmail = Regex.IsMatch(s, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (!startWith0)
+            {
+                return "SDT bắt đầu bằng 0";
+            }
+            else if (!isNumber)
+            {
+                return "SDT chỉ chứa số";
+            }
+            else if (!isLength)
+            {
+                return "SDT có 10 số";
+            }
+            return "";
+
+
         }
+
 
     }
 }
